b50bd4ae3fa821fc094626a11f249d128e0340ff
bb8ZgjUjU <joharrywork@gmail.com>
2024-02-01 22:54:05 +0900

    02012024

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-function checkURL (abc) {
-    let string = abc.value;
+// contact email checker //
+
+function checkURL (email) {
+
+    let string = email.value;
     if (!~string.indexOf("http")) {
         string = "http://" + string;
     } 
-    abc.value = string;
-    return abc
+    email.value = string;
+    return email
+}
+
+
+// Top Button //
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10 ) {
+        mybutton.style.display = "block";   
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
